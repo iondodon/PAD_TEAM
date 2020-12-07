8b4c70b851306340b1f947f5c4e4d666ef4bab38
Diana <mdiannna@yahoo.com>
2020-12-07 13:51:38 +0200

    deleted old gateway

--- gateway/lib/gateway.ex
+++
@@ -1,7 +0,0 @@
-defmodule Gateway do
-  use Application
-
-  def start(_type, _args) do
-    Gateway.Supervisor.start_link()
-  end
-end
--- gateway/lib/gateway/http_client.ex
+++
@@ -1,8 +0,0 @@
-defmodule Gateway.HttpClient do
-    use HTTPoison.Base
-
-    def process_request_body(body) do
-        Jason.encode!(body)
-    end
-
-end
